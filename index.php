5efbabae7730905fbfa5479b27fcf69d9ace397f
clément <sanchez.clmnt@gmail.com>
2018-01-09 15:28:31 +0100

    jquery

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://dl.frontapp.com/libs/frontjs.min.js"></script>
     <script type="text/javascript" src="main.js"></script>
 </head>
--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+var conversation;
 Front.on('conversation', function (data) {
-    console.log('Conversation', data.conversation);
-    console.log('Contact', data.contact);
-    console.log('Message', data.message);
-    console.log('OtherMessages', data.otherMessages);
+
     conversation = data.conversation;
 });
+
+$.ajax({
+    url: 'fichier.php',
+    data: 'maVariable='+ conversation,
+   /* success: function(reponse) {
+        alert(reponse); // reponse contient l'affichage du fichier PHP (soit echo)
+    }*/
+});
+
